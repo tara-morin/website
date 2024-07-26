ad6550a8ed9973ccef54bfa01666cfda73f0ab19
tara-morin <tara.v.morin@gmail.com>
2024-07-26 17:04:23 -0400

    Update page.tsx
    testing font size

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         My Portfolio
       </h1>
-      <p className="mb-4">
+      <p className="mb-2">
         {`I'm a rising 4th year at the University of Virginia, double majoring in Computer Science and Cognitive Science.`}
       </p>
     </section>
