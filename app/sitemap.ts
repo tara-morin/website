73580e21c1772cf239fd0a831d8adb28db4b3216
tara-morin <tara.v.morin@gmail.com>
2024-07-26 21:26:50 +0000

    fixing blog post links

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
+    url: `${baseUrl}/resume/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
 
