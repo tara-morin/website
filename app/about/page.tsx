3315e8ed6d5efe4a9f2c33ea35125792db48aea1
tara-morin <tara.v.morin@gmail.com>
2024-07-26 21:19:25 +0000

    uh

---
+++ app/about/page.tsx
@@ -0,0 +1,4 @@
+import { notFound } from 'next/navigation'
+import { CustomMDX } from 'app/components/mdx'
+import { formatDate, getBlogPosts } from 'app/resume/utils'
+import { baseUrl } from 'app/sitemap'
