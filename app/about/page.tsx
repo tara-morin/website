cb9c089d0253b0e0f1e88787c4b0ef238ad3e18e
tara-morin <tara.v.morin@gmail.com>
2024-07-26 21:21:56 +0000

    added descriptions

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,3 +2,15 @@ import { notFound } from 'next/navigation'
 import { CustomMDX } from 'app/components/mdx'
 import { formatDate, getBlogPosts } from 'app/resume/utils'
 import { baseUrl } from 'app/sitemap'
+export default function Page() {
+    return (
+      <section>
+        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+          About Me
+        </h1>
+        <p className="mb-2">
+          {`I am a rising 4th year at the University of Virginia, double majoring in Computer Science and Cognitive Science. `}
+        </p>
+      </section>
+    )
+  }
--- app/ux/page.tsx
+++ app/ux/page.tsx
@@ -2,3 +2,15 @@ import { notFound } from 'next/navigation'
 import { CustomMDX } from 'app/components/mdx'
 import { formatDate, getBlogPosts } from 'app/resume/utils'
 import { baseUrl } from 'app/sitemap'
+export default function Page() {
+    return (
+      <section>
+        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+          UX Design Projects
+        </h1>
+        <p className="mb-2">
+          {`Below are links to the 2 large UX design projects I have completed.`}
+        </p>
+      </section>
+    )
+  }
