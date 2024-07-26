18575f79c75347ca8e8a54f8945de2060e24d43f
tara-morin <tara.v.morin@gmail.com>
2024-07-26 22:01:54 +0000

    fixed resume main page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -8,8 +8,13 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Resume</h1>
       <BlogPosts />
+    
+    <p className="mb-2">
+      {'Linked below is the most recent version of my resume.'}
+    </p>
+
     </section>
   )
 }
