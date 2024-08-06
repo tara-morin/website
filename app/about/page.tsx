93f7f792e018ad1c6b89d15398d4e21d551403f1
tara-morin <tara.v.morin@gmail.com>
2024-08-06 16:07:08 +0000

    added bio information

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
           About Me
         </h1>
         <p className="mb-2">
-          {`I am a rising 4th year at the University of Virginia, double majoring in Computer Science and Cognitive Science. `}
+          {`I am a rising 4th year at the University of Virginia, double majoring in Computer Science and Cognitive Science. Post graduation, my goal is to find a job working UX design, software design, or software development. My hope is that my degree in Computer Science will provide the technical skills necessary for me to succeed in the software industry, and my degree in Cognitive Science will give me better insight into how a user thinks about and interacts with an app or website.  `}
         </p>
         <Image src={headshot} alt="A headshot of Tara at the UVA Rotunda" width="200" height="300"/>
       </section>
