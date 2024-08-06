c6db9199841d07ba30225a39bd5b509424496574
tara-morin <tara.v.morin@gmail.com>
2024-08-06 16:27:15 +0000

    added images and text to UX

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,10 +12,10 @@ export default function Page() {
         <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
           About Me
         </h1>
+        <Image src={headshot} alt="A headshot of Tara at the UVA Rotunda" width="200" height="300"/>
         <p className="mb-2">
           {`I am a rising 4th year at the University of Virginia, double majoring in Computer Science and Cognitive Science. Post graduation, my goal is to find a job working UX design, software design, or software development. My hope is that my degree in Computer Science will provide the technical skills necessary for me to succeed in the software industry, and my degree in Cognitive Science will give me better insight into how a user thinks about and interacts with an app or website.  `}
         </p>
-        <Image src={headshot} alt="A headshot of Tara at the UVA Rotunda" width="200" height="300"/>
       </section>
     )
   }
--- app/ux/page.tsx
+++ app/ux/page.tsx
@@ -2,6 +2,9 @@ import { notFound } from 'next/navigation'
 import { CustomMDX } from 'app/components/mdx'
 import { formatDate, getBlogPosts } from 'app/resume/utils'
 import { baseUrl } from 'app/sitemap'
+import Image from 'next/image';
+import kitchen from 'app/ux/KitchenSync (2).jpg'
+import rumobi from 'app/ux/rumobi.jpg'
 export default function Page() {
     return (
       <section>
@@ -19,8 +22,10 @@ export default function Page() {
           >
             <p className="ml-2 h-7">RuMoBi</p>
           </a>
-      
-
+          <Image src={rumobi} alt="The opening screen of RuMoBi" width="150" height="350"/>
+          <p>
+            RuMoBi is the design project I created in a 3 person team during my Creative Interaction Design intensive in the summer of 2024. Our task was to create an app that would enhance the experience of tourists in the city of Heraklion, Crete where we were residing. We conducted interviews with over 30 tourists from a variety of countries, and found that many tourists were bored by the way history was presented at the various histocial sites near the city. Therefore we created RuMoBi, an interactive game where users can collect characters and artifacts as they walk around the city. Each of the weeks on the site reflects a design iteration: Week 1 was comprised of interviews and a low fidelity paper prototype, Week 2 featured a medium prototype created in Figma, and Week 3 had a high fidelity prototype. 
+          </p>
         
           <a
             className="text-color1"
@@ -28,6 +33,15 @@ export default function Page() {
             href="https://sites.google.com/view/teamchimps/home"
           >
             <p className="ml-2 h-7">KitchenSync</p>
+          </a>
+          <Image src={kitchen} alt="The logo of KitchenSync." width="400" height="100"/>
+          <p>
+            KitchenSync is a semester long design project in which I led a team of 5 as project manager. The app was designed to streamline all activities related to diet, from making a grocery list, finding a nearby store, montioring nutrients and calories, and tracking a grocery budget. We developed KitchenSync through the 4 phases of the UX lifecycle: Research/Analysis, Design, Prototyping, and Evaluation. 
+          </p>
+          <a
+            className="text-color2"
+          >
+            <p className="ml-2 h-7">Note: currently the links for Phase 3 and 4 are not active due to the Google site switching domains. I am in contact with the owner of the site to get this issue resolved.</p>
           </a>
         
       </section>
