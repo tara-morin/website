1685b3757592731a4970a509d6a8d7f8527f98f2
tara-morin <tara.v.morin@gmail.com>
2024-12-24 17:37:03 -0500

    commented out time display

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,42 +14,42 @@ import { renderContent } from "@/app/resources";
 import { useTranslations } from "next-intl";
 import { i18n } from "@/app/resources/config";
 
-type TimeDisplayProps = {
-    timeZone: string;
-    locale?: string;  // Optionally allow locale, defaulting to 'en-GB'
-};
+// type TimeDisplayProps = {
+//     timeZone: string;
+//     locale?: string;  // Optionally allow locale, defaulting to 'en-GB'
+// };
 
-const TimeDisplay: React.FC<TimeDisplayProps> = ({ timeZone, locale = 'en-GB' }) => {
-    const [currentTime, setCurrentTime] = useState('');
+// const TimeDisplay: React.FC<TimeDisplayProps> = ({ timeZone, locale = 'en-GB' }) => {
+//     const [currentTime, setCurrentTime] = useState('');
 
-    useEffect(() => {
-        const updateTime = () => {
-            const now = new Date();
-            const options: Intl.DateTimeFormatOptions = {
-                timeZone,
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
-                hour12: false,
-            };
-            const timeString = new Intl.DateTimeFormat(locale, options).format(now);
-            setCurrentTime(timeString);
-        };
+//     useEffect(() => {
+//         const updateTime = () => {
+//             const now = new Date();
+//             const options: Intl.DateTimeFormatOptions = {
+//                 timeZone,
+//                 hour: '2-digit',
+//                 minute: '2-digit',
+//                 second: '2-digit',
+//                 hour12: false,
+//             };
+//             const timeString = new Intl.DateTimeFormat(locale, options).format(now);
+//             setCurrentTime(timeString);
+//         };
 
-        updateTime();
-        const intervalId = setInterval(updateTime, 1000);
+//         updateTime();
+//         const intervalId = setInterval(updateTime, 1000);
 
-        return () => clearInterval(intervalId);
-    }, [timeZone, locale]);
+//         return () => clearInterval(intervalId);
+//     }, [timeZone, locale]);
 
-    return (
-        <>
-            {currentTime}
-        </>
-    );
-};
+//     return (
+//         <>
+//             {currentTime}
+//         </>
+//     );
+// };
 
-export default TimeDisplay;
+// export default TimeDisplay;
 
 export const Header = () => {
     const router = useRouter();
