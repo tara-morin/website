8ea2b46af5f93ccd00d42f2bd477963d0eb7c954
tara-morin <tara.v.morin@gmail.com>
2024-12-26 23:53:24 -0500

    change again

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,7 +8,7 @@ import { baseURL, effects, style } from '@/app/resources'
 import {Lora} from 'next/font/google';
 import { Inter } from 'next/font/google';
 import { Source_Code_Pro } from 'next/font/google';
-import { Playwrite_USA_Modern} from 'next/font/google'
+import { PlaywriteIEGuides-Regular} from 'next/font/google'
 
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, getTranslations, unstable_setRequestLocale } from 'next-intl/server';
@@ -56,7 +56,7 @@ const primary = Lora({
 	display: 'swap',
 })
 
-const secondary = Playwrite_USA_Modern({
+const secondary = PlaywriteIEGuides-Regular({
 	variable: '--font-secondary',
 	subsets: ['latin'],
 	display: 'swap',
