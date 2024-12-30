501335bda2695e4af92b1d3ea1c00461bd7dd899
tara-morin <tara.v.morin@gmail.com>
2024-12-30 01:50:09 -0500

    new format from chat

--- src/app/[locale]/resume/page.tsx
+++ src/app/[locale]/resume/page.tsx
@@ -1,73 +1,54 @@
 import { promises as fs } from "fs";
 import path from "path";
 import { Flex, Button } from "@/once-ui/components";
-import {remark} from "/vercel/path0/node_modules/remark/index";
+import { remark } from "remark";
 import html from "remark-html";
 
-// async function getMarkdownContent() {
-//     // Read the markdown file
-//     const filePath = path.join(process.cwd(), "public", "resume.md");
-//     const fileContents = await fs.readFile(filePath, "utf8");
-
-//     // Parse the markdown into HTML
-//     const parsedMarkdown = await remark().use(html).process(fileContents);
-//     return parsedMarkdown.toString();
-// }
-
-export default async function Resume() {
-    // const markdownHtml = await getMarkdownContent();
-
-    return (
-        <Flex>
-          <Flex maxWidth=10
-          maxHeight=10
-          >
-          <a
-                href="/TMresume.pdf"
-                download="TMresume.pdf"
-                style={{
-                    padding: "1rem 2rem",
-                    backgroundColor: "#0070f3",
-                    color: "#fff",
-                    borderRadius: "5px",
-                    textDecoration: "none",
-                    fontWeight: "bold",
-                    fontSize: "1.2rem",
-                }}
-            >
-                Download my Resume
-            </a>
-          </Flex>
-          <Flex fillWidth>
-            {/* <div
-                dangerouslySetInnerHTML={{ __html: markdownHtml }}
-                style={{ maxWidth: "800px", margin: "auto", padding: "2rem" }}
-            /> */}
+export default function Resume() {
+  return (
+    <Flex>
+      <Flex maxWidth={10} maxHeight={10}>
+        <a
+          href="/TMresume.pdf"
+          download="TMresume.pdf"
+          style={{
+            padding: "1rem 2rem",
+            backgroundColor: "#0070f3",
+            color: "#fff",
+            borderRadius: "5px",
+            textDecoration: "none",
+            fontWeight: "bold",
+            fontSize: "1.2rem",
+          }}
+        >
+          Download my Resume
+        </a>
+      </Flex>
+      <Flex fillWidth>
         <div
-      style={{
-        maxWidth: "800px",
-        margin: "auto",
-        padding: "20px",
-        fontFamily: "Calibri",
-        lineHeight: 1.6,
-        color: "#333",
-      }}
-    >
-      <h1
-        style={{
-          fontSize: "2rem",
-          fontWeight: "bold",
-          textAlign: "center",
-        }}
-      >
-        Tara Morin
-      </h1>
-      <p style={{ textAlign: "center" }}>
-        tmn7vs@virginia.edu | 304-620-6551 | Purcellville, Virginia
-      </p>
-      <hr style={{ margin: "20px 0", border: "1px solid #ccc" }} />
-
-      <h2
+          style={{
+            maxWidth: "800px",
+            margin: "auto",
+            padding: "20px",
+            fontFamily: "Calibri",
+            lineHeight: 1.6,
+            color: "#333",
+          }}
+        >
+          <h1
+            style={{
+              fontSize: "2rem",
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+          >
+            Tara Morin
+          </h1>
+          <p style={{ textAlign: "center" }}>
+            tmn7vs@virginia.edu | 304-620-6551 | Purcellville, Virginia
+          </p>
+          <hr style={{ margin: "20px 0", border: "1px solid #ccc" }} />
+          <h2
         style={{
           fontSize: "1.5rem",
           fontWeight: "bold",
@@ -208,9 +189,8 @@ export default async function Resume() {
           Framer, Adobe Photoshop, Audacity, Reaper
         </li>
       </ul>
-    </div>
-    </Flex>  
-        </Flex>
-        
-    );
+        </div>
+      </Flex>
+    </Flex>
+  );
 }
