fce9dacdc6e6df37c5e904eca80baeacc62d69bd
tara-morin <tara.v.morin@gmail.com>
2024-12-29 20:23:48 -0500

    updated tsx

--- src/app/[locale]/ux/page.tsx
+++ src/app/[locale]/ux/page.tsx
@@ -1,30 +1,21 @@
-import { Flex } from "@/once-ui/components";
 import { promises as fs } from "fs";
 import path from "path";
+import { Flex } from "@/once-ui/components";
 import remark from "remark";
 import html from "remark-html";
-import { unstable_setRequestLocale } from "next-intl/server";
 
-export async function getStaticProps() {
+async function getMarkdownContent() {
     // Read the markdown file
     const filePath = path.join(process.cwd(), "public", "resume.md");
     const fileContents = await fs.readFile(filePath, "utf8");
 
     // Parse the markdown into HTML
     const parsedMarkdown = await remark().use(html).process(fileContents);
-    const markdownHtml = parsedMarkdown.toString();
-
-    return {
-        props: {
-            markdownHtml,
-        },
-    };
+    return parsedMarkdown.toString();
 }
 
-export default function Resume(
-    { params: { locale }, markdownHtml }: { params: { locale: string }; markdownHtml: string }
-) {
-    unstable_setRequestLocale(locale);
+export default async function Resume() {
+    const markdownHtml = await getMarkdownContent();
 
     return (
         <Flex fillWidth>
