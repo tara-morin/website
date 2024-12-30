093fe623727876800accf52768634be386a6648a
tara-morin <tara.v.morin@gmail.com>
2024-12-29 21:25:17 -0500

    removed a

--- src/app/[locale]/resume/page.tsx
+++ src/app/[locale]/resume/page.tsx
@@ -20,7 +20,6 @@ export default async function Resume() {
     return (
         <Flex fillWidth>
             <Button
-                as="a"
                 href="/TMresume.pdf"
                 download="TMresume.pdf"
                 style={{
