debfc5f1d9cfc44d10a8ceed4406d3d60cc2845a
tara-morin <tara.v.morin@gmail.com>
2025-01-09 04:18:34 -0500

    oneproject class

--- src/components/ux/OneProject.tsx
+++ src/components/ux/OneProject.tsx
@@ -1,36 +1,34 @@
 import { getPosts } from '@/app/utils/utils';
 import { Flex } from '@/once-ui/components';
-
 import { ProjectCard } from '@/components';
 
 interface ProjectsProps {
-    slug: string;
+    slug: string; // Use slug to identify the specific project to display
     locale: string;
 }
 
 export function OneProject({ slug, locale }: ProjectsProps) {
     let allProjects = getPosts(['src', 'app', '[locale]', 'ux', 'projects', locale]);
 
+    // Find the specific project based on the slug
     const project = allProjects.find(post => post.slug === slug);
 
-    const displayedProjects = range
-        ? project.slice(range[0] - 1, range[1] ?? project.length)
-        : project;
+    if (!project) {
+        return <div>Project not found</div>; // Handle case if the project isn't found
+    }
 
     return (
         <Flex
             fillWidth gap="xl" marginBottom="40" paddingX="l"
             direction="column">
-            {displayedProjects.map((post) => (
-                <ProjectCard
-                    key={post.slug}
-                    href={`ux/${post.slug}`}
-                    images={post.metadata.images}
-                    title={post.metadata.title}
-                    description={post.metadata.summary}
-                    content={post.content}
-                    avatars={post.metadata.team?.map((member) => ({ src: member.avatar })) || []}/>
-            ))}
+            <ProjectCard
+                key={project.slug}
+                href={`ux/${project.slug}`}
+                images={project.metadata.images}
+                title={project.metadata.title}
+                description={project.metadata.summary}
+                content={project.content}
+                avatars={project.metadata.team?.map((member) => ({ src: member.avatar })) || []}/>
         </Flex>
     );
 }
