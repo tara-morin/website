74edc3573aa3831fd77a944636be1b625cf58190
tara-morin <tara.v.morin@gmail.com>
2025-01-27 21:48:02 -0500

    changed columns to 2

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -200,14 +200,12 @@ export default function About(
                                     </Flex>
                                 ))}
                             </Flex>
-                            <Grid border="brand-medium" columns={4} gap="24" padding="24" radius="l"  background="brand-medium">
+                            <Grid border="brand-medium" columns={2} gap="24" padding="24" radius="l"  background="brand-medium">
                                 {about.studies.courses.map((course, index)=>(
-                                    // <Flex key={`${course.name}-${index}`}>
                                         <Text
                                             id={course.name}>
                                         {course.name}
                                         </Text>
-                                    // </Flex>
                                 ))}
                             </Grid>      
                         </>
