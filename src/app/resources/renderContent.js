988d973ad7714cfd4d6861fcc462b4318879b19f
tara-morin <tara.v.morin@gmail.com>
2025-06-09 17:03:41 -0400

    added web dev import to content rendering

--- src/app/resources/renderContent.js
+++ src/app/resources/renderContent.js
@@ -1,4 +1,4 @@
-import { person, newsletter, social, home, about, machine_learning, ux, resume } from './content';
+import { person, newsletter, social, home, about, machine_learning, ux, web_dev, resume } from './content';
 import { createI18nContent } from './content-i18n';
 import { i18n } from './config';
 
